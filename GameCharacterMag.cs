3b35181fc4e2dc6c4d41fe461d5e7e49849ac096
Darek <darekt@abmicro.pl>
2021-11-11 20:51:07 +0100

    Use base data variables added : base afrer method declaration

---
+++ GameCharacterMag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDziedziczenie
+{
+    class GameCharacterMag : GameCharacter
+    {
+        public GameCharacterMag(string name, int healPointsHP, int manaPointsMP) : base (name,healPointsHP)
+        {
+            this.ManaPointsPm = manaPointsMP;
+        }
+
+        public int ManaPointsPm;
+    }
+}
