937b55e527143a2ad39c7121b1c3837ce65f29c6
Darek <darekt@abmicro.pl>
2021-11-11 20:04:43 +0100

    Add project files.

---
+++ BlueBody.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDziedziczenie
+{
+    class BlueBody
+    {
+        public string Name { get; set; }
+
+        public void RoundMove()
+        {
+            Console.WriteLine("Każde ciało niebieskie ma ruch obrotowy");
+        }
+    }
+}
